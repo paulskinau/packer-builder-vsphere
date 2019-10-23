65991ee39a387ed74a88195cb2d9580f3ad88fb3
EC2 Default User <ec2-user@ip-172-16-1-74.ap-southeast-2.compute.internal>
2019-10-23 05:03:39 +0000

    adding more debug

--- driver/datastore.go
+++ driver/datastore.go
@@ -96,7 +96,7 @@ func (ds *Datastore) UploadFile2(src, dst string, host string) (string, error) {
                 ctx = ds.ds.HostContext(ctx, h.host)
         }
 
-        return "Upload File Step", ds.ds.UploadFile(ctx, src, dst, &p)
+        return fmt.Sprintf("Upload File Step %s->%s", src,dst), ds.ds.UploadFile(ctx, src, dst, &p)
 }
 
 func (ds *Datastore) UploadFile(src, dst string, host string) error {
